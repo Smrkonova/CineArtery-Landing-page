1b069fcd9cc23618c9af647c4bdca0f87c89db26
surya <ssuryareddy2277@gmail.com>
2025-08-29 22:16:15 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
-
 // Load header
 fetch('/./shared/header.html')
     .then(res => res.text())
-    .then(data => document.getElementById('header').innerHTML = data);
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+
+        const header = document.querySelector(".header");
+        let lastScroll = 0; // keep track of last scroll position
+
+        window.addEventListener("scroll", function () {
+            let currentScroll = window.scrollY;
+
+            if (currentScroll > lastScroll && currentScroll > 50) {
+                // scrolling DOWN
+                header.classList.add("scrolled");
+            } else if (currentScroll < lastScroll) {
+                // scrolling UP
+                header.classList.remove("scrolled");
+            }
+
+            lastScroll = currentScroll;
+        });
+    });
 
 // Load footer
 fetch('/./shared/footer.html')
